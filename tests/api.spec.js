15956c5801cb794fa586dc7b4fcff674f003594c
Olga-KIrienko <bobovara@gmail.com>
2026-04-05 13:30:50 +0300

    Revert "first commit"
    This reverts commit fa70790619d4300ae8c9b94e272d3a6951f5b1f9.

--- tests/api.spec.js
+++ tests/api.spec.js
@@ -15,14 +15,13 @@ test.describe('API-тесты для Restful-booker', () => {
     },
     "additionalneeds" : "Breakfast"
 };
+ 
+let bookingId = 1079;
 
-  let id; //переменная для объявления во втором тесте
+let authToken = "266d8bdadfb746f";
 
-  let authToken// переменная для токена
 
-  test('@api Создание бронирования', async ({ request }) => {
-    //Данные для бронирования:
-    
+  test('Создание бронирования', async ({ request }) => {
     // Отправляем POST-запрос
     const response = await request.post(`${baseURL}/booking`, {
         data : bookingData,
@@ -40,88 +39,88 @@ test.describe('API-тесты для Restful-booker', () => {
     // Проверка 3: В ответе есть объекты с ключом 'bookingid'
     expect(responseBody).toHaveProperty('bookingid');
 
-    //Объявляем значение переменной для дальнейшего использования
-    id = responseBody.bookingid
 
   });
 
-  test('@api Получение информации о бронировании', async ({ request }) => {
-
-    // Отправляем get-запрос
-    const response = await request.get(`${baseURL}/booking/${id}`);
-
-    // Проверка 1: Статус-код ответа
-    console.log(`Статус-код: ${response.status()}`);
-    expect(response.status()).toBe(200);
-
-       // Проверка 2: В ответе cоответствуют данным в первом тесте
-    const responseBody = await response.json();
-    console.log('Тело ответа:', responseBody);
-    expect(responseBody).toMatchObject(bookingData);
-
+  test('Получение информации о бронировании', async ({ request }) => {
+  
+      // Отправляем get-запрос
+      const response = await request.get(`${baseURL}/booking/${bookingId}`);
+  
+      // Проверка 1: Статус-код ответа
+      console.log(`Статус-код: ${response.status()}`);
+      expect(response.status()).toBe(200);
+  
+         // Проверка 2: В ответе cоответствуют данным в первом тесте
+      const responseBody = await response.json();
+      console.log('Тело ответа:', responseBody);
+      expect(responseBody).toMatchObject(bookingData);
+  
+        });
+  
+      test('Обновление бронирования', async ({ request }) => {
+      
+      const authData = {
+      "username" : "admin",
+      "password" : "password123",
+      };
+  
+      const authResponse = await request.post(`${baseURL}/auth`, {
+          data : authData
       });
-
-    test('@api Обновление бронирования', async ({ request }) => {
-    
-    const authData = {
-    "username" : "admin",
-    "password" : "password123",
-    };
-
-    const authResponse = await request.post(`${baseURL}/auth`, {
-        data : authData
-    });
-
-    const authBody = await authResponse.json()
-
-    authToken = authBody.token
-
-    const newBookingData = {
-    "firstname" : "John",
-    "lastname" : "Brown",
-    "totalprice" : 666,
-    "depositpaid" : true,
-    "bookingdates" : {
-        "checkin" : "2018-01-01",
-        "checkout" : "2019-01-01"
-    },
-    "additionalneeds" : "Breakfast"
-};
-
-    // Отправляем put-запрос
-    const response = await request.put(`${baseURL}/booking/${id}`, {
-        data: newBookingData, headers: {
-    'Cookie': `token=${authToken}`
-    }
-    });
-
-    // Проверка 1: Статус-код ответа
-    console.log(`Статус-код: ${response.status()}`);
-    expect(response.status()).toBe(200);
-
-       // Проверка 2: В ответе содержатся обновленные данные
-    const responseBody = await response.json();
-    console.log('Тело ответа:', responseBody);
-    expect(responseBody).toMatchObject(newBookingData);
-
+  
+      const authBody = await authResponse.json()
+  
+      const authToken = authBody.token;
+      console.log(authToken);
+  
+  
+      const newBookingData = {
+      "firstname" : "John",
+      "lastname" : "Brown",
+      "totalprice" : 666,
+      "depositpaid" : true,
+      "bookingdates" : {
+          "checkin" : "2018-01-01",
+          "checkout" : "2019-01-01"
+      },
+      "additionalneeds" : "Breakfast"
+  };
+  
+      // Отправляем put-запрос
+      const response = await request.put(`${baseURL}/booking/${bookingId}`, {
+          data: newBookingData, headers: {
+      'Cookie': `token=${authToken}`
+      }
       });
-
-    test('@api Удаление бронирования', async ({ request }) => {
-    
-    // Отправляем delete-запрос
-    const response = await request.delete(`${baseURL}/booking/${id}`, {
-        headers: {
-        'Cookie': `token=${authToken}`
-    }
-    });
-
-    // Проверка 1: Статус-код ответа
-    console.log(`Статус-код: ${response.status()}`);
-    expect(response.status()).toBe(201);
-
-    // Проверка 2: дополнительная, что бронирования нет
-    const addCheck = await request.get(`${baseURL}/booking/${id}`);
-    console.log(`Статус-код: ${addCheck.status()}`);
-    expect(addCheck.status()).toBe(404);
-    });  
+  
+      // Проверка 1: Статус-код ответа
+      console.log(`Статус-код: ${response.status()}`);
+      expect(response.status()).toBe(200);
+  
+         // Проверка 2: В ответе содержатся обновленные данные
+      const responseBody = await response.json();
+      console.log('Тело ответа:', responseBody);
+      expect(responseBody).toMatchObject(newBookingData);
+  
+        });
+  
+      test('Удаление бронирования', async ({ request }) => {
+      
+      // Отправляем delete-запрос
+      const response = await request.delete(`${baseURL}/booking/${bookingId}`, {
+          headers: {
+          'Cookie': `token=${authToken}`
+      }
+      });
+  
+      // Проверка 1: Статус-код ответа
+      console.log(`Статус-код: ${response.status()}`);
+      expect(response.status()).toBe(201);
+  
+      // Проверка 2: дополнительная, что бронирования нет
+      const addCheck = await request.get(`${baseURL}/booking/${bookingId}`);
+      console.log(`Статус-код: ${addCheck.status()}`);
+      expect(addCheck.status()).toBe(404);
+      });  
 });
